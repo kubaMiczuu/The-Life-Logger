6e77dc084680b489336555e9de8574b110d3e1ab
kubaMiczuu <kubamiczuu@gmail.com>
2026-03-16 09:32:23 +0100

    feat: add CORS configuration

---
+++ server-spring/src/main/java/com/thelifelogger/serverspring/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.thelifelogger.serverspring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+class WebConfiguration extends WebMvcConfigurationSupport {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
